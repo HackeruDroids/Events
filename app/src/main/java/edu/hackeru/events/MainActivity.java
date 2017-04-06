8ae913ffc2aa5428d886feab3f899feba143a47c
hackerudroids <tomerjava@gmail.com>
2017-04-06 19:40:05 +0300

    Shouting the text

--- app/src/main/java/edu/hackeru/events/MainActivity.java
+++ app/src/main/java/edu/hackeru/events/MainActivity.java
@@ -2,6 +2,7 @@ package edu.hackeru.events;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -9,22 +10,23 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private EditText etShout;
+    private Button btnShout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditText etShout = (EditText) findViewById(R.id.etShout);
-        Button btnShout = (Button) findViewById(R.id.btnShout);
+        etShout = (EditText) findViewById(R.id.etShout);
+        btnShout = (Button) findViewById(R.id.btnShout);
 
         //btnShout.setText(R.string.hello);
         btnShout.setOnClickListener(this);
-        etShout.setText("");
-
     }
 
     @Override
     public void onClick(View view) {
-        Toast.makeText(this, "Hello, Events", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, etShout.getText().toString().toUpperCase(), Toast.LENGTH_SHORT).show();
     }
 }
